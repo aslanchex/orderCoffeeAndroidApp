8ceaffd6bd9e2c6ac8ccce24c2cff696026f5c1f
aslanchex <aslan.kashiev@mail.ru>
2020-04-22 00:59:32 +0300

    Добавлена стилизация

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -1,30 +1,37 @@
 package com.example.android.justjava;
 
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.text.NumberFormat;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-    int pcsOfCups = 0;
+    int pcsOfCups = 1;
+    Toast toast;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
     /**
      * Содержит поля:
      * price - стоимость заказа
      * hasWhippedCream - Добавил ли клент взбитые сливки
      * hasChocolate - добавил ли клиент шоколад
-     *
+     * <p>
      * Стоимость кофе без добавок - 50 р, сливки - 15 р, шоколад - 25р.
+     * <p>
+     * Если поле для ввода имени пустое, то заказ не совершается.
      */
     public void submitOrder(View view) {
         int price = calculatePrice();
@@ -42,18 +49,21 @@ public class MainActivity extends AppCompatActivity {
         name = editText.getText().toString();
 
         //Log.v("MainActivity", "The price is " +price);
-        if (hasWhippedCream) {price += pcsOfCups * 15;}
-        if (hasChocolate) {price += pcsOfCups * 25;}
-
+        if (hasWhippedCream) {
+            price += pcsOfCups * 15;
+        }
+        if (hasChocolate) {
+            price += pcsOfCups * 25;
+        }
         if (!name.isEmpty()) {
             createOrderSummary(name, pcsOfCups, price, hasWhippedCream, hasChocolate);
-        }
-        else {
+        } else {
             editText.setHintTextColor(getResources().getColor(R.color.hintTextColor));
         }
     }
+
     /**
-     * Calculates the price of the order.
+     * Метод вычисляет стоимость чашек кофе
      */
     private int calculatePrice() {
         int price = pcsOfCups * 50;
@@ -62,22 +72,42 @@ public class MainActivity extends AppCompatActivity {
 
     /**
      * Метод выводит на экран Имя, количество чашек , стоимость заказа и "Thank you"
-     * @param name имя клиента
-     * @param quantity количество чашек кофе
+     *
+     * @param name       имя клиента
+     * @param quantity   количество чашек кофе
      * @param totalPrice стоимость заказа
      */
     private void createOrderSummary(String name, int quantity, int totalPrice, boolean hasWhippedCream, boolean hasChocolate) {
+        Resources res = this.getResources();
+
         displayMessage(
-                "Имя: " + name + "\n" +
-                "Добавить взбитые сливки? " + (hasWhippedCream ? "Да":"Нет") + "\n" +
-                "Добавить шоколад? " + (hasChocolate ? "Да":"Нет") + "\n" +
-                "Количество: " + quantity + "\n" +
-                "Стоимость: " + NumberFormat.getCurrencyInstance().format(totalPrice) + "\n" +
-                "Спасибо за заказ!"
+                res.getString(R.string.order_summary_name, name) + "\n" +
+                res.getString(R.string.order_summary_whipped_cream) + " "  + (hasWhippedCream ? "Да" : "Нет") + "\n" +
+                res.getString(R.string.order_summary_chocolate) + " "  + (hasChocolate ? "Да" : "Нет") + "\n" +
+                res.getString(R.string.order_summary_quantity) + " " + quantity + "\n" +
+                res.getString(R.string.order_summary_price, NumberFormat.getCurrencyInstance().format(totalPrice)) + "\n" +
+                res.getString(R.string.thank_you)
         );
+        /*Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("*\/*");
+        intent.putExtra(Intent.EXTRA_EMAIL, "aslan.kashiev@mail.ru");
+        intent.putExtra(Intent.EXTRA_SUBJECT, R.string.order_summary_email_subject);
+        intent.putExtra(Intent.EXTRA_TEXT,
+                R.string.order_summary_name + name + "\n" +
+                        R.string.order_summary_whipped_cream + (hasWhippedCream ? "Да" : "Нет") + "\n" +
+                        String.valueOf(R.string.order_summary_chocolate).toString() + (hasChocolate ? "Да" : "Нет") + "\n" +
+                        R.string.order_summary_quantity + quantity + "\n" +
+                        R.string.order_summary_price + NumberFormat.getCurrencyInstance().format(totalPrice) + "\n" +
+                        R.string.thank_you
+        );
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }*/
     }
+
     /**
      * Этот метод выводит
+     *
      * @param message на экран
      */
     private void displayMessage(String message) {
@@ -85,22 +115,42 @@ public class MainActivity extends AppCompatActivity {
         priceTextView.setText(message);
     }
 
+    /**
+     * Метод увеличивает счетчик кофе на 1
+     *
+     * @param view
+     */
     public void increment(View view) {
-        pcsOfCups++;
-        display(pcsOfCups);
+        if (pcsOfCups < 100) {
+            pcsOfCups++;
+            display(pcsOfCups);
+        } else {
+            toast = Toast.makeText(getApplicationContext(), "Нельзя заказать больше 100 чашек", Toast.LENGTH_SHORT);
+            toast.show();
+        }
     }
 
+    /**
+     * Метод уменьшает счетчик кофе на 1
+     *
+     * @param view
+     */
     public void decrement(View view) {
-        if (pcsOfCups > 0)
+        if (pcsOfCups > 1) {
             pcsOfCups--;
             display(pcsOfCups);
+        } else {
+            toast = Toast.makeText(getApplicationContext(), "Невозможно заказать меньше 1 чашки", Toast.LENGTH_SHORT);
+            toast.setGravity(Gravity.BOTTOM, 0, 0);
+            toast.show();
+        }
     }
+
     /**
-     * This method displays the given quantity value on the screen.
+     * Метод выводит количество заказанных чашек
      */
     private void display(int number) {
         TextView quantityTextView = (TextView) findViewById(R.id.quantityNumber);
         quantityTextView.setText("" + number);
     }
-
 }
