88beaa677dfc7bfef491b558fa18ea3b3627f61b
aslanchex <aslan.kashiev@mail.ru>
2020-04-21 18:21:13 +0300

    Commit

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.android.justjava;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import java.text.NumberFormat;
@@ -10,18 +12,45 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     int pcsOfCups = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
     /**
-     * This method is called when the order button is clicked.
+     * Содержит поля:
+     * price - стоимость заказа
+     * hasWhippedCream - Добавил ли клент взбитые сливки
+     * hasChocolate - добавил ли клиент шоколад
+     *
+     * Стоимость кофе без добавок - 50 р, сливки - 15 р, шоколад - 25р.
      */
     public void submitOrder(View view) {
         int price = calculatePrice();
-        //displayPrice(price);
-        createOrderSummary("Aslan Kashiev", pcsOfCups, price);
+        boolean hasWhippedCream = false;
+        boolean hasChocolate = false;
+        String name = "";
+
+        CheckBox checkBoxWhippedCream = (CheckBox) findViewById(R.id.whipped_cream_chechbox); // Чекбокс взбитые сливки
+        hasWhippedCream = checkBoxWhippedCream.isChecked();
+
+        CheckBox checkBoxChocolate = (CheckBox) findViewById(R.id.chocolate_chechbox); // Чекбокс шоколад
+        hasChocolate = checkBoxChocolate.isChecked();
+
+        EditText editText = (EditText) findViewById(R.id.input_name_view);
+        name = editText.getText().toString();
+
+        //Log.v("MainActivity", "The price is " +price);
+        if (hasWhippedCream) {price += pcsOfCups * 15;}
+        if (hasChocolate) {price += pcsOfCups * 25;}
+
+        if (!name.isEmpty()) {
+            createOrderSummary(name, pcsOfCups, price, hasWhippedCream, hasChocolate);
+        }
+        else {
+            editText.setHintTextColor(getResources().getColor(R.color.hintTextColor));
+        }
     }
     /**
      * Calculates the price of the order.
@@ -37,12 +66,14 @@ public class MainActivity extends AppCompatActivity {
      * @param quantity количество чашек кофе
      * @param totalPrice стоимость заказа
      */
-    private void createOrderSummary(String name, int quantity, int totalPrice) {
+    private void createOrderSummary(String name, int quantity, int totalPrice, boolean hasWhippedCream, boolean hasChocolate) {
         displayMessage(
                 "Имя: " + name + "\n" +
+                "Добавить взбитые сливки? " + (hasWhippedCream ? "Да":"Нет") + "\n" +
+                "Добавить шоколад? " + (hasChocolate ? "Да":"Нет") + "\n" +
                 "Количество: " + quantity + "\n" +
                 "Стоимость: " + NumberFormat.getCurrencyInstance().format(totalPrice) + "\n" +
-                "Thank you!"
+                "Спасибо за заказ!"
         );
     }
     /**
@@ -71,4 +102,5 @@ public class MainActivity extends AppCompatActivity {
         TextView quantityTextView = (TextView) findViewById(R.id.quantityNumber);
         quantityTextView.setText("" + number);
     }
+
 }
